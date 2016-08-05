4586028ffe4e8b0b2f139c3fbd0375aebd2af53b
sdavidk <davidkohno500@gmail.com>
2016-08-05 09:57:53 -0400

    initial commit

---
+++ GreetUP/Models/Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GreetUP.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+        [MaxLength(50)]
+        public string EventName { get; set; }
+        [MaxLength(100)]
+        public string Time { get; set; }
+        [MaxLength(50)]
+        public string Date { get; set; }
+        public string Description { get; set; }
+        public int ApplicationUserID { get; set; }
+
+        //Navigation Properties 
+        //linking ApplicationUserId to ApplicationUser table
+        public virtual ApplicationUser ApplicationUser { get; set; }
+        //linking Event table to RSVP table
+        public virtual ICollection<RSVP> RSVPs { get; set; }
+    }
+}
---
+++ GreetUP/Models/RSVP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreetUP.Models
+{
+    public class RSVP
+    {
+        public int RSVPID { get; set; }
+        public int EventId { get; set; }
+        public int ApplicationUserId { get; set; }
+
+
+    //Navigation Properties
+    //linking EventId to Event table---linking ApplicationUserId to ApplicationUser table
+        public virtual Event Event { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
